8968cb1cc334fa8e9490ca2c8c5043c0e13cacd8
Ahadov Ilhomjon <akhadovilhom@gmail.com>
2021-09-22 18:10:07 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi there');
+})
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`server  running on ${port} port`)
+})
